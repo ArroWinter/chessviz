e0fdcfb414bb76b2901cc7399b684893eee53f88
Anna <anutiykus@gmail.com>
2018-05-27 15:47:18 +0700

    Add all .h libraryes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "p.h"
+#include "P.h"
+#include "board.h"
+#include "horse.h"
+#include "king.h"
+#include "el.h"
+#include "queen.h"
+#include "rook.h"
 
 int main()
 {
