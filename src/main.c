5a1379251793b8defc0ff82d3a69374242694e20
Anna <anutiykus@gmail.com>
2018-05-27 12:46:23 +0700

    Add main function in main file

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("clear");
+    char s[9][9], k[5];
+    int z1 = 0, z2 = 0, z3 = 0, z4 = 0, z5 = 0, z6 = 0, z7 = 0, z8 = 0, z9 = 0, z10 = 0, 
+z11 = 0, z12 = 0, z13 = 0, z14 = 0, flag1 = 0;
+    return 0;
+}
